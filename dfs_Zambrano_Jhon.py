2a54e07029ada2fca9e5465cb04e41d3d25c0f9c
Jhon Zambrano <jhonmacias1999@gmail.com>
2022-05-28 15:53:57 -0500

    PrimerComits - importamos queue

---
+++ dfs_Zambrano_Jhon.py
@@ -0,0 +1,8 @@
+
+'''
+Es especialmente util en la programacion en hilo cuando la informacion 
+debe intercambiarse de forma segura entre varios subprocesos. 
+La clase Queue de este
+modulo implementa toda la semantica de bloqueo necesaria.
+'''
+from queue import Queue
